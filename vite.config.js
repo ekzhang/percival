239e70951079d7830e2bb8056c5002601bc04b30
Eric Zhang <ekzhang1@gmail.com>
2021-11-11 11:50:12 -0500

    Configure Vite to allow top-level await

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+    chunkSizeWarningLimit: 1500,
+  },
   plugins: [svelte()],
 });
