f2031b196b610abf575a18258df7dafb62246169
Eric Zhang <ekzhang1@gmail.com>
2021-11-24 23:49:52 -0500

    Fix dependency pre-bundling issue in tests

--- src/lib/runtime.ts
+++ src/lib/runtime.ts
@@ -1,6 +1,16 @@
 import { compile } from "percival-wasm";
 import Worker from "./worker?worker";
 
+// Needed to fix dependency pre-bundling issue in mocha-vite-puppeteer.
+//
+// This line is necessary because `immutable` is not present in the non-worker
+// bundle. This makes tests get confused because they discover the library mid-
+// execution and reload the page, breaking Puppeteer.
+//
+// The extra import does not affect performance or bundle size because of
+// automatic tree-shaking optimizations.
+import "immutable";
+
 interface CancellablePromise<T> extends Promise<T> {
   cancel: () => void;
 }
@@ -34,9 +44,11 @@ export function build(src: string): CompilerResult {
           rejectCb = reject;
           worker.addEventListener("message", (event) => {
             resolve(event.data);
+            worker.terminate();
           });
           worker.addEventListener("error", (event) => {
             reject(event.error);
+            worker.terminate();
           });
           worker.postMessage({ type: "source", code });
           worker.postMessage({ type: "eval", deps });
